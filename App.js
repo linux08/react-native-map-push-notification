5dd1a0e98416110018e7260c283a2ac27f95d78a
linux08 <abimbola130@gmail.com>
2018-10-05 12:59:22 +0100

    init project

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView from "react-native-maps";
+
+let { width, height } = Dimensions.get("window");
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+export default class App extends Component {
+  render() {
+    return (
+      <MapView
+        style={{
+          flex: 1
+        }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        showsUserLocation
+        showsCompass
+        loadingEnabled
+      />
+    );
+  }
+}
+
